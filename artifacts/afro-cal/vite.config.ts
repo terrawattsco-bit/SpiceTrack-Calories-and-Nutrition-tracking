6358fc5103611bc8ea2805c60c1dd583f32df8a3
terrawattsco-bit <terrawatts.co@gmail.com>
2026-04-05 12:16:18 -0400

    Update vite.config.ts

--- artifacts/afro-cal/vite.config.ts
+++ artifacts/afro-cal/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: "./", // Explicitly set the root to the current folder
   base: "./", 
   plugins: [react()],
   resolve: {
